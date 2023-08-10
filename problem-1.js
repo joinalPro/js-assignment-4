a0147826e0d6e54dc5a76bab035d7cc4010d61e1
smartlycoding <joinalzx@gmail.com>
2023-08-10 19:41:15 +0600

    p1 done

---
+++ problem-1.js
@@ -0,0 +1,12 @@
+// problem number 1
+
+function cubeNumber(number) {
+    const frist = Math.pow(3, 3);
+    const num = Math.pow(4, 3);
+    console.log(frist);
+    return num;
+}
+let myNumber = 27;
+let mysecond = 64;
+let cubeNumbers = cubeNumber(myNumber, mysecond);
+console.log(cubeNumbers);
